64fd8c74b722305964c38bc25c518487da3e5fdf
zakariadjendli <202157206@cegepsherbrooke.qc.ca>
2025-04-28 14:45:17 -0400

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,15 +1,22 @@
 fichier = open ("Dossier_externe.json", "r")
+fichier.write("Liste électoral :\n")
+fichier.write("Nathaniel\n")
+fichier.write("Felix\n")
+fichier.write("Zakaria\n")
+fichier.close()
+
+fichier = open ("Dossier_externe.json", "r")
 lignes = fichier.readlines()
 print(lignes)
 candidat1 = "Nathaniel"
 candidat2 = "Felix"
 candidat3 = "Zakaria"
 
-if lignes[0] == "Nathaniel":
+if lignes[1] == "Nathaniel":
     candidat1 += 1
-if lignes[1] == "Felix":
+if lignes[2] == "Felix":
     candidat2 += 1
-if lignes[2] == "Zakaria":
+if lignes[3] == "Zakaria":
     candidat3 += 1
 if candidat1 > candidat2 and candidat3:
     print("Candidat 1 a gagné")
