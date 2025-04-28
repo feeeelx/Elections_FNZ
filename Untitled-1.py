acf6521ab7bd38f14dded55b8878b95f81532d98
zakariadjendli <202157206@cegepsherbrooke.qc.ca>
2025-04-28 15:11:54 -0400

    modif

--- Untitled-1.py
+++ Untitled-1.py
@@ -8,19 +8,25 @@ candidat1 = "Nathaniel"
 candidat2 = "Felix"
 candidat3 = "Zakaria"
 
-if lignes == "Nathaniel":
-    candidat1 += 1
-elif lignes == "Felix":
-    candidat2 += 1
-elif lignes == "Zakaria":
-    candidat3 += 1
 
+votes_candidat1 = 0
+votes_candidat2 = 0
+votes_candidat3 = 0
 
-if candidat1 >= candidat2 and candidat3:
+for ligne in lignes:
+    ligne = ligne.strip()  
+    if ligne == "Nathaniel":
+        votes_candidat1 += 1
+    elif ligne == "Felix":
+        votes_candidat2 += 1
+    elif ligne == "Zakaria":
+        votes_candidat3 += 1
+
+if votes_candidat1 >= votes_candidat2 and votes_candidat1 >= votes_candidat3:
     print("Candidat 1 a gagné")
-elif candidat2 >= candidat1 and candidat3:
+elif votes_candidat2 >= votes_candidat1 and votes_candidat2 >= votes_candidat3:
     print("Candidat 2 a gagné")
-elif candidat3 >= candidat1 and candidat2:
+elif votes_candidat3 >= votes_candidat1 and votes_candidat3 >= votes_candidat2:
     print("Candidat 3 a gagné")
 else:
     print("Il y a égalité")
