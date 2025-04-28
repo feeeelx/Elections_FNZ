197d75bd891c598bf3d36fd786f1bec41ce8750b
zakariadjendli <202157206@cegepsherbrooke.qc.ca>
2025-04-28 15:06:28 -0400

    a

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,28 +1,26 @@
-fichier = open ("Dossier_externe.json", "r")
-fichier.write("Liste électoral :\n")
-fichier.write("Nathaniel\n")
-fichier.write("Felix\n")
-fichier.write("Zakaria\n")
-fichier.close()
 
-fichier = open ("Dossier_externe.json", "r")
+
+fichier = open ("exemple.json", "r")
 lignes = fichier.readlines()
-print(lignes)
+
+#print(lignes)
 candidat1 = "Nathaniel"
 candidat2 = "Felix"
 candidat3 = "Zakaria"
 
-if lignes[1] == "Nathaniel":
+if lignes == "Nathaniel":
     candidat1 += 1
-if lignes[2] == "Felix":
+elif lignes == "Felix":
     candidat2 += 1
-if lignes[3] == "Zakaria":
+elif lignes == "Zakaria":
     candidat3 += 1
-if candidat1 > candidat2 and candidat3:
+
+
+if candidat1 >= candidat2 and candidat3:
     print("Candidat 1 a gagné")
-elif candidat2 > candidat1 and candidat3:
+elif candidat2 >= candidat1 and candidat3:
     print("Candidat 2 a gagné")
-elif candidat3 > candidat1 and candidat2:
+elif candidat3 >= candidat1 and candidat2:
     print("Candidat 3 a gagné")
 else:
     print("Il y a égalité")
