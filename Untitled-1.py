7df8dfc097acd338a4ebe56022c1728ef62da211
zakariadjendli <202157206@cegepsherbrooke.qc.ca>
2025-04-28 14:35:44 -0400

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,7 +4,12 @@ print(lignes)
 candidat1 = "Nathaniel"
 candidat2 = "Felix"
 candidat3 = "Zakaria"
-
+if lignes[0] == "Nathaniel":
+    candidat1 += 1
+if lignes[1] == "Felix":
+    candidat2 += 1
+if lignes[2] == "Zakaria":
+    candidat3 += 1
 if candidat1 > candidat2 and candidat3:
     print("Candidat 1 a gagné")
 elif candidat2 > candidat1 and candidat3:
